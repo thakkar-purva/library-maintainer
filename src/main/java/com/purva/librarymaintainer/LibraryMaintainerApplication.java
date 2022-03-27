bd07394c1c5afd539d9031630f796f71ce0eca13
Purva <pjthakkar3192@gmail.com>
2022-03-27 14:47:59 -0600

    project setup.

---
+++ src/main/java/com/purva/librarymaintainer/LibraryMaintainerApplication.java
@@ -0,0 +1,13 @@
+package com.purva.librarymaintainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMaintainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryMaintainerApplication.class, args);
+    }
+
+}
